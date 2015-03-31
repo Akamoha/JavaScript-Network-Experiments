ed35da0693073f4ee3668d1850bf476c1876361c
Akshay Damle <akshaydamle95@gmail.com>
2015-03-31 13:16:49 +0530

    Helper code modified to include RGB and alpha (opacity) values

--- Raphael with JSON input file/Helper code/jsoncoverter.py
+++ Raphael with JSON input file/Helper code/jsoncoverter.py
@@ -1,8 +1,7 @@
 print "data = '[",
 for i in range(43734):
 	A = map(float, raw_input().split())
-	print '{"x":'+str(A[0])+', "y":'+str(A[1])+"},",
+	print '{"x":'+str(A[1])+', "y":'+str(A[2])+', "radius":'+str(A[3])+', "r":'+str(A[4])+', "g":'+str(A[5])+', "b":'+str(A[6])+"},",
 A = map(float, raw_input().split())
-print '{"x":'+str(A[0])+', "y":'+str(A[1])+"}",
+print '{"x":'+str(A[1])+', "y":'+str(A[2])+', "radius":'+str(A[3])+', "r":'+str(A[4])+', "g":'+str(A[5])+', "b":'+str(A[6])+"}",
 print "]';"
-	
