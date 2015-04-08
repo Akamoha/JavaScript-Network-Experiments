94bf18d40b4c067200988b0bddd6d6605b925c28
Akshay Damle <akshaydamle95@gmail.com>
2015-04-08 22:55:18 +0530

    Reduced node sizes and opacity values

--- Raphael with Materialize/code.js
+++ Raphael with Materialize/code.js
@@ -51,26 +51,58 @@ $(document).ready(function() {
 	{   
 			var line = paper.path( ["M", startX, startY, "L", endX, endY ] );
 			line.attr("stroke-width", "0.25");
-			line.attr("opacity", 7*alpha);
+			line.attr("opacity", 1.5*alpha);
 			line.attr("stroke", Raphael.rgb(r*255,g*255,b*255));
 			line.translate(0.1, 0.1);
 	}
 	
-	function randomIntFromInterval(min,max)
-	{
-		return Math.floor(Math.random()*(max-min+1)+min);
+query_prefix="http://localhost:5000/astopo/api";
+	
+	function getNormalQuery() {
+		snapshot=$("#snapshot")[0].value;
+		if(!snapshot){
+			snapshot = "2013.03.01.0200";
+		};
+		outdegree=$("#outdegree")[0].value;
+		country=$("#country")[0].value;
+		edgetype=$("#edgetype")[0].value;
+		return query_prefix+"/normal/snapshot="+snapshot+"&outdegree="+outdegree+"&country="+country+"&edgetype="+edgetype;
 	}
 	
+	function getBipartiteQuery() {
+		snapshot=$("#snapshot")[0].value;
+		if(!snapshot){
+			snapshot = "2013.03.01.0200";
+		};
+		set1=$("#set1")[0].value;
+		set2=$("#set2")[0].value;
+		return query_prefix+"/normal/snapshot="+snapshot+"&set1="+set1+"&set2="+set2;
+	}
+	
+	function getASCentricQuery() {
+		snapshot=$("#snapshot")[0].value;
+		if(!snapshot){
+			snapshot = "2013.03.01.0200";
+		};
+		asnumber=$("#asnumber")[0].value;
+		return query_prefix+"/normal/snapshot="+snapshot+"&asnumber="+asnumber;
+	}
+		
 	$("#normalRedraw").bind('click', function () { 
-		alert('Normal Graph Redraw Clicked'); 
+		//$.get(getNormalQuery(), function(json) {
+		//	paper.clear();
+		//	drawEdges(json.data2,json.E);
+		//	drawNodes(json.data,json.N);
+		//});
+		alert(getNormalQuery());
 	});
 	
 	$("#bipartiteRedraw").bind('click', function () { 
-		alert('Bipartite Graph Redraw Clicked'); 
+		alert(getBipartiteQuery());
 	});
 	
 	$("#ascentricRedraw").bind('click', function () {
-		alert('AS-Centric Graph Redraw Clicked'); 
+		alert(getASCentricQuery());
 	});
 	
 	var nodeMgr = new NodeManager(paper);
@@ -88,59 +120,64 @@ $(document).ready(function() {
 	var LX1 = 10;
 	var RX = 0.6584415;
 	
-	var mydata = JSON.parse(data2);
+	drawEdges(JSON.parse(data2), E);
+	drawNodes(JSON.parse(data), N);
 
-	for(i = 0; i < E; i++) {
-		var givenStartX = mydata[i].sx;
-		var givenStartY = mydata[i].sy;
-		
-		//var startX = (givenStartX - LX)/RX + LX1;
-		//var startY = (givenStartY - LY)/RY + LY1;
-		
-		//var startX = givenStartX / 2 + 300;
-		//var startY = givenStartY / 2 + 100;
-		
-		var startX = givenStartX + XOFFSET;
-		var startY = givenStartY + YOFFSET;
-		
-		var givenEndX = mydata[i].ex;
-		var givenEndY = mydata[i].ey;
-		
-		//var endX = (givenEndX - LX)/RX + LX1;
-		//var endY = (givenEndY - LY)/RY + LY1;
-		
-		//var endX = givenEndX / 2 + 300;
-		//var endY = givenEndY / 2 + 100;
-		
-		var endX = givenEndX + XOFFSET;
-		var endY = givenEndY + YOFFSET;
-		
-		var alpha = mydata[i].alpha;
-		var r = mydata[i].r;
-		var g = mydata[i].g;
-		var b = mydata[i].b;
-		
-		nodeMgr.connectNodes(startX, startY, endX, endY, alpha, r, g, b);
+	function drawEdges(jsondata,E) {
+		var mydata = jsondata
+		for(i = 0; i < E; i++) {
+			var givenStartX = mydata[i].sx;
+			var givenStartY = mydata[i].sy;
+			
+			//var startX = (givenStartX - LX)/RX + LX1;
+			//var startY = (givenStartY - LY)/RY + LY1;
+			
+			//var startX = givenStartX / 2 + 300;
+			//var startY = givenStartY / 2 + 100;
+			
+			var startX = givenStartX + XOFFSET;
+			var startY = givenStartY + YOFFSET;
+			
+			var givenEndX = mydata[i].ex;
+			var givenEndY = mydata[i].ey;
+			
+			//var endX = (givenEndX - LX)/RX + LX1;
+			//var endY = (givenEndY - LY)/RY + LY1;
+			
+			//var endX = givenEndX / 2 + 300;
+			//var endY = givenEndY / 2 + 100;
+			
+			var endX = givenEndX + XOFFSET;
+			var endY = givenEndY + YOFFSET;
+			
+			var alpha = mydata[i].alpha;
+			var r = mydata[i].r;
+			var g = mydata[i].g;
+			var b = mydata[i].b;
+			
+			nodeMgr.connectNodes(startX, startY, endX, endY, alpha, r, g, b);
+		}	
 	}
 	
-	var mydata = JSON.parse(data);
-	
-	for(i = 0; i < N; i++) {
-		var givenx = mydata[i].x;
-		var giveny = mydata[i].y;
-		
-		//var x = (givenx - LX)/RX + LX1;
-		//var y = (giveny - LY)/RY + LY1;
-		
-		//var x = givenx / 2 + 300;
-		//var y = giveny / 2 + 100;
-		
-		var x = givenx + XOFFSET;
-		var y = giveny + YOFFSET;
-		
-		var radius = mydata[i].radius;
-		var node_style = { fill: Raphael.rgb(mydata[i].r, mydata[i].g, mydata[i].b), stroke: 'black', 'stroke-width': 1 };
-		
-		nodeMgr.addNode("o", x, y, radius, node_style);
+	function drawNodes(jsondata,N) {
+		var mydata = jsondata;
+		for(i = 0; i < N; i++) {
+			var givenx = mydata[i].x;
+			var giveny = mydata[i].y;
+			
+			//var x = (givenx - LX)/RX + LX1;
+			//var y = (giveny - LY)/RY + LY1;
+			
+			//var x = givenx / 2 + 300;
+			//var y = giveny / 2 + 100;
+			
+			var x = givenx + XOFFSET;
+			var y = giveny + YOFFSET;
+			
+			var radius = mydata[i].radius*0.3;
+			var node_style = { fill: Raphael.rgb(mydata[i].r, mydata[i].g, mydata[i].b), stroke: 'black', 'stroke-width': 1 };
+						
+			nodeMgr.addNode("o", x, y, radius, node_style);
+		}	
 	}
 });
